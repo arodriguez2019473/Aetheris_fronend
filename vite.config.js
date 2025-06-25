afc77df0638b593f05463357fc49138d197dc7fd
arodriguez2019473 <arodriguez2019473@kinal.edu.gt>
2025-06-25 14:01:28 -0600

    se hace una configuracion en el fix para vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // importante si está fallando en Vercel
+  base: "./", // <- esto es clave para Vercel
 });
-// Este archivo configura Vite para usar React y establece la base del proyecto.
-// Asegúrate de que el archivo esté en la raíz del proyecto y que Vite

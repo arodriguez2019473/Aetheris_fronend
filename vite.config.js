538e2c2c39d058c2b48f8e6b81bab583c6fbf27a
arodriguez2019473 <arodriguez2019473@kinal.edu.gt>
2025-06-25 13:56:31 -0600

    se vuelve hacer push para ver errores en vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
+  base: "./", // importante si está fallando en Vercel
 });
-
+// Este archivo configura Vite para usar React y establece la base del proyecto.
+// Asegúrate de que el archivo esté en la raíz del proyecto y que Vite
